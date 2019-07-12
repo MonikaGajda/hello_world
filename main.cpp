89833400dd003a28a94c051206cf898097ae2864
MonikaZ <monika.zelechowska@o2.pl>
2019-07-12 15:14:13 +0200

    Travis CI first test

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ main()
 {
 	std::cout<<"Monika Zelechowska"<<std::endl;
 	std::cout<<"hello world!"<<std::endl;
+	std::cout<<"------------------"<<std::endl;
+	std::cout<<"Additional changes"<<std::endl;
 
 return 0;
 }
