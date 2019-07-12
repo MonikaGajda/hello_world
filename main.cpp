63c2f99f25fbe35fa53113908571cb090ae21ac9
MonikaZ <monika.zelechowska@o2.pl>
2019-07-12 16:22:26 +0200

    Travis CI eight test

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ main()
 	std::cout<<"Monika Zelechowska"<<std::endl;
 	std::cout<<"hello world!"<<std::endl;
 	std::cout<<"------------------"<<std::endl;
-	std::cout<<"Correct working"<<std::endl;
+	std::cout<<"Wrong line"<std::endl;
 
 return 0;
 }
