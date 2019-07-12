8414da4e95e7a01ba486c25af8d760830f930734
MonikaZ <monika.zelechowska@o2.pl>
2019-07-12 23:34:50 +0200

    Revert "Wrong commit"
    This reverts commit 4e857c1bc812e6670702a2cdab73fc728814370c.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main()
 	std::cout<<"Monika Zelechowska"<<std::endl;
 	std::cout<<"hello world!"<<std::endl;
 	std::cout<<"------------------"<<std::endl;
-	std::cout<<"Wrong line"<std::endl;
+	std::cout<<"Additional changes"<<std::endl;
 
 return 0;
 }
