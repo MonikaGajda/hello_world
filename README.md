bd3c71b3c009dd4131a075a164f182907c801d3b
MonikaZ <monika.zelechowska@o2.pl>
2019-07-12 16:19:30 +0200

    Travis CI seventh test

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # hello_world
-https://travis-ci.org/MonikaZelechowska/hello_world.svg?branch=master
+[![Build Status](https://travis-ci.org/MonikaZelechowska/hello_world.svg?branch=master)](https://travis-ci.org/MonikaZelechowska/hello_world)
--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@ main()
 	std::cout<<"Monika Zelechowska"<<std::endl;
 	std::cout<<"hello world!"<<std::endl;
 	std::cout<<"------------------"<<std::endl;
-	std::cout<<"Additional changes"<<std::endl;
-	std::cout<<"Wrong line"std::endl;
+	std::cout<<"Correct working"<<std::endl;
 
 return 0;
 }
